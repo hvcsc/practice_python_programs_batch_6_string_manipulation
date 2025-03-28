eebd9b2126d2d03b6ed3b8b8c441e36e58f82cd0
hvcsc <jrhcmll13@gmail.com>
2025-03-28 18:14:42 +0800

    print output

--- batch_6/6.6_ljust.py
+++ batch_6/6.6_ljust.py
@@ -2,5 +2,12 @@
 specifies in function parameter. Create a program that do the same functionality without using ljust() function."""
 
 #define ljust
+def l_just(string, width):
+    return string + " " * (width - len(string))
+
 #ask user for input
+string = input("Enter string: ")
+width = int(input("Enter width: "))
+
 #print output
+print(l_just(string, width))
