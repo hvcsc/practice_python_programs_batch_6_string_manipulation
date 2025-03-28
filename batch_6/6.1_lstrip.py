eb6d1b1a287c24e1946f884e9b24a775eecdda69
hvcsc <jrhcmll13@gmail.com>
2025-03-28 15:02:25 +0800

    print output

--- batch_6/6.1_lstrip.py
+++ batch_6/6.1_lstrip.py
@@ -2,4 +2,7 @@
 Create a program that do the same functionality without using lstrip() function."""
 
 #get user input
+user = input("Enter anything with space characters at the beginning: ").strip()
+
 #print output
+print(user)
