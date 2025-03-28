74a1e31f97515cf2572ff00c963152ad8e06b2b7
hvcsc <jrhcmll13@gmail.com>
2025-03-28 15:34:36 +0800

    print lower case

--- batch_6/6.3_lower.py
+++ batch_6/6.3_lower.py
@@ -2,4 +2,7 @@
 Create a program that do the same functionality without using lower() function."""
 
 #get user input and convert to lower
+user = input("Enter a string: ").casefold()
+
 #print lower case
+print(user)
