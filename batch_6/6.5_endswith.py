d89338eb7e4583eeef7d0548c9a5f34f72f15fad
hvcsc <jrhcmll13@gmail.com>
2025-03-28 18:05:23 +0800

    check suffix if true or false

--- batch_6/6.5_endswith.py
+++ batch_6/6.5_endswith.py
@@ -2,5 +2,19 @@
 Create a program that do the same functionality without using endswith() function."""
 
 #define suffix
+def ends_with(string, suffix):
+    position = len(string) - len(end)
+    if string.find(end, position) == position:
+        return True
+    else:
+        return False
+
 #user input
+string = input("Enter a string: ")
+end = input("\nEnter the suffix to check: ")
+
 #call function and print
+if ends_with(string, end):
+    print("\nTrue")
+else:
+    print("\nFalse")
