2701597bc768162c6d1c0d4a3c28ed70044fd48f
hvcsc <jrhcmll13@gmail.com>
2025-03-28 18:11:37 +0800

    pseudocode

---
+++ batch_6/6.6_ljust.py
@@ -0,0 +1,6 @@
+"""Prog06. ljust() add space characters at the end of the string to complete the number of characters
+specifies in function parameter. Create a program that do the same functionality without using ljust() function."""
+
+#define ljust
+#ask user for input
+#print output
