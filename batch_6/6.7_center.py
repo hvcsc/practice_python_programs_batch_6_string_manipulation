f7448c8222a1ec9edbe7c7c0fdd7af2a8f6f7486
hvcsc <jrhcmll13@gmail.com>
2025-03-28 20:07:05 +0800

    print output

--- batch_6/6.7_center.py
+++ batch_6/6.7_center.py
@@ -3,5 +3,13 @@ string to print the string at the center. Create a program that do the same func
 without using center() function."""
 
 #define center()
+def center(string, width):
+    spaces = (width - len(string)) // 2
+    return ' ' * spaces + string + ' ' * (width - len(string) - spaces)
+
 #ask input
+user = input("Enter a string: ")
+wid = int(input("Enter the total width: "))
+
 #print output
+print(center(user, wid))
