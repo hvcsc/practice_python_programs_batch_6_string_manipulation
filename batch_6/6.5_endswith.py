d6755469c5a9672089bd735374bc7c14bebb4f0f
hvcsc <jrhcmll13@gmail.com>
2025-03-28 17:58:26 +0800

    pseudocode

---
+++ batch_6/6.5_endswith.py
@@ -0,0 +1,6 @@
+"""Prog05. endswith() check if the string end part matches the function parameter.
+Create a program that do the same functionality without using endswith() function."""
+
+#define suffix
+#user input
+#call function and print
