191c10485eb9562eab2705551760df6060722a66
hvcsc <jrhcmll13@gmail.com>
2025-03-28 20:27:51 +0800

    pseudocode

---
+++ batch_6/6.9_swapcase.py
@@ -0,0 +1,6 @@
+"""Prog09. swapcase() reverse the casing of each of the character of the string.
+Create a program that do the same functionality without using swapcase() function."""
+
+#define condition for swapcase
+#ask input
+#print
