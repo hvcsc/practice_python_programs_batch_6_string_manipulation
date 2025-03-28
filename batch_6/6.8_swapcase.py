6337074e32dc444cb83b96c71ee0484ab42d6e82
hvcsc <jrhcmll13@gmail.com>
2025-03-28 20:15:32 +0800

    print swapcase

--- batch_6/6.8_swapcase.py
+++ batch_6/6.8_swapcase.py
@@ -2,5 +2,19 @@
 Create a program that do the same functionality without using swapcase() function."""
 
 #define condition for swapcase
+def swap_case(string):
+    swap = ""
+    for i in string:
+        if i.islower():
+            swap += i.upper()
+        elif i.isupper():
+            swap += i.lower()
+        else:
+            swap += i
+    return swap
+
 #ask input
+user = swap_case(input("Enter string: "))
+
 #print
+print(user)
