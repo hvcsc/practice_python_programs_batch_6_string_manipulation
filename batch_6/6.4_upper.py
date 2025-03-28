953cdd8e855b6d0c6716d4b358d36dee97aae37e
hvcsc <jrhcmll13@gmail.com>
2025-03-28 16:19:34 +0800

    check and print

--- batch_6/6.4_upper.py
+++ batch_6/6.4_upper.py
@@ -2,4 +2,10 @@
 Create a program that do the same functionality without using isupper() function."""
 
 #ask input
+user = input("Enter a string: ")
+
 #check if upper and print
+if user.islower():
+    print("False")
+else:
+    print("True")
