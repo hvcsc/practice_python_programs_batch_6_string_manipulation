5e32a2a9138fd2cfe838bb8c485cf9701160b7ad
hvcsc <jrhcmll13@gmail.com>
2025-03-28 15:25:23 +0800

    pseudocode

---
+++ batch_6/6.2_remove_prefix.py
@@ -0,0 +1,6 @@
+"""Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter.
+Create a program that do the same functionality without using removeprefix() function."""
+
+#get user input
+#set conditions
+#print without the prefix
