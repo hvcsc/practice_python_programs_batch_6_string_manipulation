9236138345f19214cbaf7dd6b53e673a3d3028e5
hvcsc <jrhcmll13@gmail.com>
2025-04-01 07:45:12 +0800

    pseudocode

---
+++ batch_7/7.1_rstrip.py
@@ -0,0 +1,6 @@
+"""Prog01. rstrip() remove the space characters at the end of the string.
+Create a program that do the same functionality without using rstrip() function."""
+
+#ask user input
+#remove space characters at the end
+#print output
