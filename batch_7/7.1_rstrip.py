45dd9649c0db91593cfbbc204d9251c0eb01cd39
hvcsc <jrhcmll13@gmail.com>
2025-04-01 07:48:40 +0800

    print output without rstrip

--- batch_7/7.1_rstrip.py
+++ batch_7/7.1_rstrip.py
@@ -2,5 +2,11 @@
 Create a program that do the same functionality without using rstrip() function."""
 
 #ask user input
+user = input("Enter a string with space characters at the end: ")
+
 #remove space characters at the end
+while user.endswith(" "):
+    user = user[:-1]
+
 #print output
+print(user)
