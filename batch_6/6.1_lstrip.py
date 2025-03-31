bb95ea321ef8711e78e1b4927e7754f2ba24abd8
hvcsc <jrhcmll13@gmail.com>
2025-04-01 07:40:18 +0800

    edited program 1 - lstrip

--- batch_6/6.1_lstrip.py
+++ batch_6/6.1_lstrip.py
@@ -1,8 +1,12 @@
 """Prog01. lstrip() remove the space characters at the beginning of the string.
 Create a program that do the same functionality without using lstrip() function."""
 
-#get user input
-user = input("Enter anything with space characters at the beginning: ").strip()
+#ask input
+string = input("Enter string: ")
+
+#remove space
+while string.startswith(" "):
+    string = string[1:]
 
 #print output
-print(user)
+print(string)
