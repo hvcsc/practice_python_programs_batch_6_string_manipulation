2053868e506bcb92eedbb5d199053aef5347027d
hvcsc <jrhcmll13@gmail.com>
2025-04-01 12:03:54 +0800

    print output

--- batch_7/7.10_rindex.py
+++ batch_7/7.10_rindex.py
@@ -3,5 +3,18 @@ starting from the last character. Create a program that do the same functionalit
 without using rindex() function."""
 
 #ask input
+user = input("Enter a string: ")
+find = input("Enter the word to find: ")
+
 #check rindex
+position = -1
+for i in range(len(user) - len(find), -1, -1):
+    if user[i:i + len(find)] == find:
+        position = i
+        break
+
 #print output
+if position == -1:
+    print("Not found")
+else:
+    print(f"\nPosition of '{find}': {position}")
