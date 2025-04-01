a45e808a38c3115c3352b1a0885ed733ccee5b97
hvcsc <jrhcmll13@gmail.com>
2025-04-01 10:42:32 +0800

    print new format

--- batch_7/7.7_zfill.py
+++ batch_7/7.7_zfill.py
@@ -3,5 +3,11 @@ of characters specifies in function parameter. Create a program that do the same
 without using zfill() function."""
 
 #ask input
+num = int(input("Enter a number: "))
+form = int(input("Enter number format: "))
+
 #change format
-#print output
+num = f"{num:0{form}d}"
+
+#print format
+print(f"\nOutput: {num}")
