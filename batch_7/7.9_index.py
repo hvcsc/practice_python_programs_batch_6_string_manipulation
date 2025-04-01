c81f30cff4b5b585ca7365d0d4432d1c1cb176aa
hvcsc <jrhcmll13@gmail.com>
2025-04-01 11:53:38 +0800

    edited program 9 - index

--- batch_7/7.9_index.py
+++ batch_7/7.9_index.py
@@ -2,15 +2,11 @@
 Create a program that do the same functionality without using index() function."""
 
 #ask input
-nums = list(map(int, input("Enter numbers separated by spaces: ").split()))
-find = int(input("Enter the number to count: "))
+user = input("Enter a string: ")
+find = input("Enter the character to find: ")
 
 #check first location
-position = -1
-for i in range(len(nums)):
-    if nums[i] == find:
-        position = i + 1
-        break
+position = user.find(find)
 
 #print location
 print(f"\nPosition of {find}: {position}")
