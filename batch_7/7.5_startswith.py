83f4e4474eb191ba48963db3e50e64cdbcae571b
hvcsc <jrhcmll13@gmail.com>
2025-04-01 10:24:07 +0800

    pseudocode

---
+++ batch_7/7.5_startswith.py
@@ -0,0 +1,6 @@
+"""Prog05. startswith() check if the string beginning part matches the function parameter.
+Create a program that do the same functionality without using startswith() function."""
+
+#ask input
+#check conditions
+#print output
