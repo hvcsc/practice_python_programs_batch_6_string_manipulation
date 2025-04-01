7c753487b8b9449b1af5d9a23e7089cfc503722b
hvcsc <jrhcmll13@gmail.com>
2025-04-01 11:07:55 +0800

    print count

--- batch_7/7.8_count.py
+++ batch_7/7.8_count.py
@@ -2,5 +2,14 @@
 Create a program that do the same functionality without using count() function."""
 
 #ask input
+string = input("Enter a string: ")
+sub = input("Enter the substring to count: ")
+
 #count
+count = 0
+for i in range(len(string) - len(sub) + 1):
+    if string[i:i + len(sub)] == sub:
+        count += 1
+
 #print count
+print(f"\nCount: {count}")
